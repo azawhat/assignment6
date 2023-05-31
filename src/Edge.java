154552f2e05fc40befa918475912964fd26626f2
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-31 21:20:25 +0600

    Implementing class `Edge`

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,23 @@
-public class Edge {
+class Edge<T> {
+    private Vertex<T> source;
+    private Vertex<T> destination;
+    private int weight;
+
+    public Edge(Vertex<T> source, Vertex<T> destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public Vertex<T> getSource() {
+        return source;
+    }
+
+    public Vertex<T> getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
