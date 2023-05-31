0022131ee759eeed4ed8f6c967dd8151ab32e844
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-31 21:14:37 +0600

    Implementing class `Vertex`

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,17 @@
-public class Vertex {
+import java.util.*;
+
+class Vertex<T> {
+    private T data;
+
+    public Vertex(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
 }
